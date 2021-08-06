994b6ab09799f1785120de137c21e5118da9dbee
SatvinderSingh5110 <keeratjitkaur@gmail.com>
2021-08-07 00:06:07 +0800

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,57 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4  l:grid-cols-5 gap-y-10 px-32
+        py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+            
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not real site</p>
+            <p>This is a preety awesome clone</p>
+            <p>Referrals accepted</p>
+            <p>Papafam</p>
+            
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>Satvinder's React</p>
+            <p>Presents</p>
+            <p>Zero to Hero Full Stack</p>
+            <p>Join Now</p>
+            <p>Airbnb Clone</p>
+            
+            </div>
+                
+                
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust and Safety</p>
+            <p>Say Hi Youtube</p>
+            <p>Easter Eggs</p>
+            <p>For the Win</p>
+            
+            </div>
+
+                
+            
+            
+            </div>
+      
+
+    );
+}
+
+export default Footer;
